cc6983b01355e708290a769fa4b796d74526805e
Romain Seraphine <romainseraphine@gmail.com>
2020-09-22 17:57:54 +0200

    Ecrire un algorithme qui demande un nombre de départ

--- exo-loop1.rb
+++ exo-loop1.rb
@@ -1,22 +1,36 @@
 # Ecrire un algorithme qui demande un nombre de départ,
-# et qui ensuite écrit la table de multiplication de ce nombre, présentée comme suit (cas où l'utilisateur entre le nombre 7) :
-i = 1
-user_number = ""
+# et qui calcule la somme des entiers jusqu’à ce nombre.
+# Par exemple, si l’on entre 5, le programme doit calculer :
 
-while i <= 10
-puts "> Entrez un nombre :"
-user_number = gets.chomp.to_i
-puts " #{user_number} x #{i} = #{user_number * i}"
-i += 1
+# 1 + 2 + 3 + 4 + 5 = 15
+
+# NB : on souhaite afficher uniquement le résultat, pas la décomposition du calcul.
+
+number_start = 5
+i = 0
+array = []
+
+while i < number_start
+  i += 1
+  array << i
 end
+puts array.sum
 
+# --------------------------
+# Ecrire un algorithme qui demande un nombre de départ,
+# et qui ensuite écrit la table de multiplication de ce nombre, présentée comme suit (cas où l'utilisateur entre le nombre 7) :
+# i = 1
 
-
+# while i <= 10
+#   puts "> Entrez un nombre :"
+#   user_number = gets.chomp.to_i
+#   puts " #{user_number} x #{i} = #{user_number * i}"
+#   i += 1
+# end
 # Table de 7 :
 # 7 x 1 = 7
 # 7 x 2 = 14
 # 7 x 3 = 21
 # …
 # 7 x 10 = 70
-
-
+# --------------------------
