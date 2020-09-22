ce4676229f8abd86242b53cb0171c3d71a652623
Romain Seraphine <romainseraphine@gmail.com>
2020-09-22 18:39:04 +0200

    Ecrire un algorithme qui demande un nombre de départ

--- exo-loop1.rb
+++ exo-loop1.rb
@@ -1,3 +1,19 @@
+# Ecrire un algorithme qui demande un nombre de départ, et qui calcule sa factorielle.
+
+# NB : la factorielle de 8, notée 8 !, vaut
+# 1 x 2 x 3 x 4 x 5 x 6 x 7 x 8
+
+factorielle_number = 8
+i = 0
+array = []
+
+while i < factorielle_number
+  i += 1
+  array << i
+end
+  p array[0]*array[1]*array[2]*array[3]*array[4]*array[5]*array[6]*array[7]
+
+# --------------------------
 # Ecrire un algorithme qui demande un nombre de départ,
 # et qui calcule la somme des entiers jusqu’à ce nombre.
 # Par exemple, si l’on entre 5, le programme doit calculer :
@@ -6,15 +22,15 @@
 
 # NB : on souhaite afficher uniquement le résultat, pas la décomposition du calcul.
 
-number_start = 5
-i = 0
-array = []
+# number_start = 5
+# i = 0
+# array = []
 
-while i < number_start
-  i += 1
-  array << i
-end
-puts array.sum
+# while i < number_start
+#   i += 1
+#   array << i
+# end
+# puts array.sum
 
 # --------------------------
 # Ecrire un algorithme qui demande un nombre de départ,
