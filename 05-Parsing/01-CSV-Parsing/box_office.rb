894e975f0980ca09cae062965222bd68c2e3d017
Romain Seraphine <romainseraphine@gmail.com>
2020-10-20 20:43:34 +0200

    Parsing box office

--- 05-Parsing/01-CSV-Parsing/box_office.rb
+++ 05-Parsing/01-CSV-Parsing/box_office.rb
@@ -1,13 +1,14 @@
 require 'csv'
-file_path = 'lib/movies.csv'
-
+file_path = 'movies.csv'
 
 def most_successful(number, max_year, file_path)
   movies = []
   CSV.foreach(file_path) do |row|
-    movies << row if row[1].to_i <= max_year
+    puts row
+  if row[1].to_i <= max_year
+    p movies << row[1]
   end
-  p movies.sort_by { |movie| - movie[2].to_i }.take(number)
+  end
 end
 
-most_successful(10, 2000, file_path)
+most_successful(6, 1999, file_path)
