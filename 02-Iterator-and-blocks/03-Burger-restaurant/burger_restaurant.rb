b79bdd0cc056d9a090c0cbd8edbbbbeb34e15b0e
Romain Seraphine <romainseraphine@gmail.com>
2020-10-24 16:41:36 +0200

    Burger restaurant

--- 02-Iterator-and-blocks/03-Burger-restaurant/burger_restaurant.rb
+++
@@ -1,5 +0,0 @@
-def burger(patty, sauce, topping)
-# returns the burger as an array of strings
-  cooked = block_given? ? yield(patty) : patty
-  ["bread", "#{cooked}", "#{sauce}", "#{topping}", "bread"]
-end
